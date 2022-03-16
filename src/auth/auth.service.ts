ff3b8b7dccbefe26037ecf1fa4bb53d5fc2b5814
Peterdinis <peterdinis0@pobox.sk>
2022-03-16 15:10:30 +0100

    Updating auth files

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { CreateUserDto, LoginUserDto, UserDto } from "../users/dto/user.dto";
+import {RegistrationStatus} from "./interfaces/register-status";
+import {UsersService} from "../users/users.service";
+import {LoginStatus} from "./interfaces/login-status";
+import {JwtService}from "@nestjs/jwt";
+import {JwtPayload} from "./interfaces/payload";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(userDto: CreateUserDto): Promise<RegistrationStatus> {
+    let status: RegistrationStatus = {
+      success: true,
+      message: 'user registered',
+    };
+
+    try {
+      await this.usersService.create(userDto);
+    } catch (err) {
+      status = {
+        success: false,
+        message: err,
+      };
+    }
+
+    return status;
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<LoginStatus> {
+    // find user in db
+    const user = await this.usersService.findByLogin(loginUserDto);
+
+    // generate and sign token
+    const token = this._createToken(user);
+
+    return {
+      username: user.username,
+      ...token,
+    };
+  }
+
+  async validateUser(payload: JwtPayload): Promise<UserDto> {
+    const user = await this.usersService.findByPayload(payload);
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+
+  private _createToken({ username }: UserDto): any {
+    const expiresIn = process.env.EXPIRESIN;
+
+    const user: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(user);
+    return {
+      expiresIn,
+      accessToken,
+    };
+  }
+}
